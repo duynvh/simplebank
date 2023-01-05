c08735c4217b83ae9a1b41f47251772a2a29624a
duynvh <nguyenvohoangduy@gmail.com>
2023-01-05 15:28:41 +0700

    Setup github ci

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -18,12 +18,11 @@ var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	testDB, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
-
 	testQueries = New(testDB)
-
 	os.Exit(m.Run())
 }
